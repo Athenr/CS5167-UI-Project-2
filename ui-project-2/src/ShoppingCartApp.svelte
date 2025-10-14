ab65c4d12508a2cc5f61eed5fb9fbcf25ebffc4e
Athenr <82354163+Athenr@users.noreply.github.com>
2025-10-14 16:30:35 -0400

    Update template

---
+++ ui-project-2/src/ShoppingCartApp.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<main>
+<div class="main-application-background">
+    <h1>Shopping Cart Application example</h1>
+</div>
+</main>
+
+<style>
+ .main-application-background {
+    background-color: #f0f0f0;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    width: 1000px;
+    height: 750px;
+ }
+  
+  h1 {
+    color: #333;
+  }
+</style>
