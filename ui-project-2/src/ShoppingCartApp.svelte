4cd3efc99df5b01ada7aa4fa379e09647d6ca020
Jack Detrick <jackdetrick6@gmail.com>
2025-10-20 16:17:06 -0400

    update

--- ui-project-2/src/ShoppingCartApp.svelte
+++ ui-project-2/src/ShoppingCartApp.svelte
@@ -2,13 +2,21 @@
   import LoginPage from "./screens/LoginPage.svelte";
     import PaymentPage from "./screens/PaymentPage.svelte";
   import ShoppingPage from "./screens/ShoppingPage.svelte";
+
+  let activePage = $state(0)
 </script>
 
 <main>
   <div class="main-application-background">
+    {#if activePage = 0}
     <LoginPage />
-    <!-- <ShoppingPage /> -->
-     <!-- <PaymentPage/> -->
+    {:else if activePage = 1}
+    <ShoppingPage />
+    {:else if activePage = 2}
+     <PaymentPage bind:activePage={activePage}/>
+     {:else}
+     <LoginPage />
+     {/if} 
   </div>
 </main>
 
